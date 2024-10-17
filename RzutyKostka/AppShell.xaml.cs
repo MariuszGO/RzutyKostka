5d86b11747cde66f39859a2555be114e2ddf2b3d
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2024-10-17 11:39:10 +0200

    Dodaj pliki projektów.

---
+++ RzutyKostka/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RzutyKostka
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RzutyKostka/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RzutyKostka
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
