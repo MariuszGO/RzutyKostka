97b64728b62310062e8151ee4374d67047900692
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2024-10-17 12:14:16 +0200

    n3

--- RzutyKostka/MainPage.xaml.cs
+++ RzutyKostka/MainPage.xaml.cs
@@ -16,14 +16,17 @@
         {
             Random los = new Random();
             int [] tab = new int [5];
+            int suma_calosc = 0;
+            int suma_gry = 0;
 
 
             for (int i = 0; i < 5; i++)
             {
 
                 tab[i] = los.Next(1,7);
-
+                suma_gry = suma_gry + tab[i];
             }
+            suma_calosc = suma_calosc + suma_gry;
 
             z1.Source = ImageSource.FromFile($"z{tab[0]}.png");
             z2.Source = ImageSource.FromFile($"z{tab[1]}.png");
