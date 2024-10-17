8482abea3e2975afe3433820e7e3f37cc3f2172d
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2024-10-17 12:10:27 +0200

    n2

--- RzutyKostka/MainPage.xaml.cs
+++ RzutyKostka/MainPage.xaml.cs
@@ -8,8 +8,32 @@
         {
             InitializeComponent();
         }
+       
 
-     
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Random los = new Random();
+            int [] tab = new int [5];
+
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                tab[i] = los.Next(1,7);
+
+            }
+
+            z1.Source = ImageSource.FromFile($"z{tab[0]}.png");
+            z2.Source = ImageSource.FromFile($"z{tab[1]}.png");
+            z3.Source = ImageSource.FromFile($"z{tab[2]}.png");
+            z4.Source = ImageSource.FromFile($"z{tab[3]}.png");
+            z5.Source = ImageSource.FromFile($"z{tab[4]}.png");
+
+
+
+        }
     }
 
 }
